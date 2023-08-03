086a63ba8ad184caaf5cb1812f2718a0b2845e7c
kamil <kamil_olszewski@op.pl>
2023-08-03 20:49:40 +0200

    add buttons(edit, delete) in printer view

--- Print_Hub/urls.py
+++ Print_Hub/urls.py
@@ -1,23 +1,9 @@
-"""
-URL configuration for HUB project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, reverse
 from Hub import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +22,6 @@ urlpatterns = [
     path('delete_printer/<int:printer_id>/', views.DeletePrinter.as_view(), name='delete_printer'),
     path('edit_printer/<int:printer_id>/', views.EditPrinter.as_view(), name='edit_printer'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
